dabb64e926951edc9efd009caba3a1f3a618c946
vijaypranesh <33872745+vijaypranesh@users.noreply.github.com>
2024-12-08 11:14:40 +0530

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-dnf -y install dnf-plugins-core
-dnf -y config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+dnf -f install dnf-plugins-core
+dnf -f config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+dnf -f install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 systemctl enable --now docker
 docker run hello-world
